2965f37a7c770e3b4daf4b67bfb068b4de970883
fiqryq <fiqrychoerudin48@gmail.com>
2023-04-30 16:47:55 +0700

    feat: init site

--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
-const button = document.querySelector('#button');
-const text = document.querySelector('#sometext');
+import { gsap } from 'gsap';
+import imagesLoaded from 'imagesloaded';
+import MouseFollower from 'mouse-follower';
 
-button.addEventListener(
-  'click',
-  () => (text.innerHTML = 'This text generated form button create.')
-);
+// Preload images
+const content = document.querySelector('section');
+const imgLoad = imagesLoaded(content);
+
+// Mouse follower
+MouseFollower.registerGSAP(gsap);
+
+const cursor = new MouseFollower({
+  speed: 0.3,
+});
+
+imgLoad.on('done', (instance) => {
+  const timeline = gsap.timeline();
+  timeline.to('.loader .title', {
+    y: -50,
+    opacity: 0,
+  });
+
+  timeline.to('.loader', {
+    scaleY: 0,
+    duration: 1,
+    ease: 'power4.out',
+  });
+});
